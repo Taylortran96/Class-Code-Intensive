d356328338a3f3b786b98b2f24ab7a3bdd374160
Taylortran96 <tranaithy96@gmail.com>
2023-07-08 22:44:32 +0700

    done

--- Practices/Buoi11/bai1-exercise/src/App.js
+++ Practices/Buoi11/bai1-exercise/src/App.js
@@ -5,6 +5,8 @@ import Pokemondata from './Components/pokemondata';
 import Pagination from './Components/pagination';
 
 function App() {
+  const [pokemons, setPokemons] = useState([])
+
   return (
     <div className="App"> 
       <PokemonList pokemon={Pokemondata.data}/>
--- Practices/Buoi11/bai1-exercise/src/Components/pokemonlist.jsx
+++ Practices/Buoi11/bai1-exercise/src/Components/pokemonlist.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import Pokemondata from "./pokemondata";
 
-export default function PokemonList() {
-    const pokemon = Pokemondata.data;
+export default function PokemonList({pokemon}) {
     return(
-        <div style={{display:"flex", flexDirection: "row", justifyContent:"space-between", margin: "0 2rem"}}>
+        <div className="pokemon-list">
             {pokemon.map ((element) => (
-                <div key={element.id}>
-                    <h1>Tên Pokemon: {element.name}</h1>
-                    <p>Chiều cao: {element.height}</p>
-                    <p>Cân nặng: {element.weight}</p>
-                </div>
+                <div key={element}>{element}</div>
             ))}
         </div>
     )
